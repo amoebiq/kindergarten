64c3890d98ff8ffa656ab99d6c12775e721c2b7b
Shuhail Kadavath <shukad333@gmail.com>
2018-02-08 14:03:50 +0530

    Health check servers

--- src/main/java/com/amoebiq/ssa/controller/ParentController.java
+++ src/main/java/com/amoebiq/ssa/controller/ParentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,9 @@ public class ParentController {
 
 		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 
+	}
+	@GetMapping("/health")
+	public ResponseEntity<String> getHealth() {
+		return null;
 	}
 }
